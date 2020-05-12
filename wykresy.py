16afedb62448b214e7e65924e95fe0fe27ebd73f
Filip Chmielewski <filip.chmielewski@codilime.com>
2020-05-12 19:40:42 +0200

    Add plots 9-15

--- wykresy.py
+++ wykresy.py
@@ -24,7 +24,7 @@ plot_stuff(
 plot_stuff(
     Series(DATASET.Raman_CCl4, "Wavenumber", "parallel", "parallel"),
     Series(DATASET.Raman_CCl4, "Wavenumber", "perpendicular", "perpendicular"),
-    x_label=Label.time,
+    x_label=Label.wavenumber,
     y_label=Label.amplitude,
     in_x_min=0,
     in_x_max=1000,
@@ -69,4 +69,71 @@ plot_stuff(
     res_x_max=1000.,
     fourierized=True,
     show_legend=True
+)
+
+# Rys 9
+plot_stuff(
+    Series(DATASET.Raman_chloroform, "Wavenumber", "parallel", "parallel"),
+    Series(DATASET.Raman_chloroform, "Wavenumber", "perpendicular", "perpendicular"),
+    x_label=Label.wavenumber,
+    y_label=Label.amplitude,
+    in_x_min=0,
+    in_x_max=3500,
+    show_legend=True
+)
+
+# Rys 12
+plot_stuff(
+    Series(DATASET.Raman_chloroform, "Wavenumber", "parallel", "Chloroform parallel"),
+    Series(DATASET.Raman_CCl4, "Wavenumber", "parallel", "CCl4 parallel"),
+    x_label=Label.wavenumber,
+    y_label=Label.amplitude,
+    in_x_min=0,
+    in_x_max=3500,
+    show_legend=True
+)
+
+# TODO: Y cutoff, by a special value (~0.12 is the new max)
+# Rys 13
+plot_stuff(
+    Series(DATASET.OKE_CCl4, "Delay", "-5C", "-5C"),
+    Series(DATASET.OKE_CCl4, "Delay", "15C", "15C"),
+    Series(DATASET.OKE_CCl4, "Delay", "35C", "35C"),
+    Series(DATASET.OKE_CCl4, "Delay", "55C", "55C"),
+    Series(DATASET.OKE_CCl4, "Delay", "70C", "70C"),
+    x_label=Label.time,
+    y_label=Label.amplitude,
+    in_x_min=-100.,
+    in_x_max=5000.,
+    show_legend=True
+)
+
+# TODO: Y cutoff: 1e-4 to 1
+# Rys 14
+plot_stuff(
+    Series(DATASET.OKE_chloroform, "Delay", "-5C", "-5C"),
+    Series(DATASET.OKE_chloroform, "Delay", "15C", "15C"),
+    Series(DATASET.OKE_chloroform, "Delay", "35C", "35C"),
+    Series(DATASET.OKE_chloroform, "Delay", "55C", "55C"),
+    x_label=Label.time,
+    y_label=Label.amplitude,
+    show_legend=True,
+    y_scale="log"
+)
+
+# Rys 15
+plot_stuff(
+    Series(DATASET.OKE_CCl4, "Delay", "-5C", "-5C"),
+    Series(DATASET.OKE_CCl4, "Delay", "15C", "15C"),
+    Series(DATASET.OKE_CCl4, "Delay", "35C", "35C"),
+    Series(DATASET.OKE_CCl4, "Delay", "55C", "55C"),
+    Series(DATASET.OKE_CCl4, "Delay", "70C", "70C"),
+    x_label=Label.wavenumber,
+    y_label=Label.amplitude,
+    in_x_min=100.,
+    res_x_min=0.,
+    res_x_max=1000.,
+    show_legend=True,
+    fourierized=True,
+    normalized=True
 )
